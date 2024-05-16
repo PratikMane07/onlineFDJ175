d5dd77830370e1847d49a19e17f06d69e1548c8c
prati <prati@Pratik>
2024-05-16 15:12:02 +0530

    by pratik 1

---
+++ Bank_Application/src/com/app/sbi/Test.java
@@ -0,0 +1,20 @@
+package com.app.sbi;
+
+public class Test {
+	
+	public void createAccount()
+	{
+		System.out.println("Account Creation");
+	}
+	
+	public void withdrawmony()
+	{
+		System.out.println("Withdraw your mony");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
